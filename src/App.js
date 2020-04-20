49378c0b44c43d57223f30938180d5efd602e4d8
Spencer Call <spencergeraldcall@gmail.com>
2020-04-20 07:25:53 -0600

    Initial style adjustments. Create basic calc layout and function of logging the value of button to the console.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+function View(props) {
+  return <div>{props.display}</div>;
+}
+
+function Button(props) {
+  return <button onClick={props.onClick}>{props.name}</button>;
+}
+
+function ButtonList(props) {
+  const renderButton = (name) => {
+    return <Button onClick={() => handleClick(name)} name={name} />;
+  };
+
+  const handleClick = (val) => {
+    console.log(val);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="button-list">
+        {renderButton('AC')}
+        {renderButton('+/-')}
+        {renderButton('%')}
+        {renderButton('÷')}
+        {renderButton('7')}
+        {renderButton('8')}
+        {renderButton('9')}
+        {renderButton('x')}
+        {renderButton('4')}
+        {renderButton('5')}
+        {renderButton('6')}
+        {renderButton('-')}
+        {renderButton('1')}
+        {renderButton('2')}
+        {renderButton('3')}
+        {renderButton('+')}
+        {renderButton('0')}
+        {renderButton('.')}
+        {renderButton('=')}
+      </div>
+    </>
+  );
+}
+
+function App() {
+  const [num, setNum] = useState(0);
+
+  return (
+    <>
+      <div className=".App">
+        <View display={num} />
+        <ButtonList />
+      </div>
+    </>
   );
 }
 
