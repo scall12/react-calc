9452c7bb6831065a362b8bc6d009d5fcbed9db40
Spencer Call <spencergeraldcall@gmail.com>
2020-04-23 13:05:44 -0600

    Refactor of calculation logic. Bug Fix #3: store state numbers as strings. Add logic for percent button.

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,72 @@
 import React, { useState } from 'react';
 import './App.css';
-import { operator, numify } from './helpers';
+import { operator } from './helpers';
 
 import View from './View';
 import ButtonList from './ButtonList';
 
 function App() {
-  const [view, setView] = useState([]);
-  const [firstNum, setFirstNum] = useState([]);
-  const [secondNum, setSecondNum] = useState([]);
+  const [view, setView] = useState('');
+  const [firstNum, setFirstNum] = useState('');
+  const [secondNum, setSecondNum] = useState('');
   const [action, setAction] = useState(null);
 
   const handleClick = (val) => {
     if (val.match(/\d/) || val === '.') {
       // number button press
       if (!action) {
-        const arr = [...firstNum, val];
-        setFirstNum(arr);
-        setView(arr);
+        const str = firstNum.concat(val);
+        setFirstNum(str);
+        setView(str);
       } else {
-        const arr = [...secondNum, val];
-        setSecondNum(arr);
-        setView(arr);
+        const str = secondNum.concat(val);
+        setSecondNum(str);
+        setView(str);
       }
     } else {
       // action button press
       if (val === 'AC' || val === 'C') {
-        setFirstNum([]);
-        setSecondNum([]);
+        setFirstNum('');
+        setSecondNum('');
         setAction(null);
-        setView([]);
-      } else if ('+-/*'.includes(val)) {
+        setView('');
+      } else if ('+-÷x'.includes(val)) {
         setAction(val);
       } else if (val === '+/-') {
-        if (!action && firstNum[0] !== '-') {
-          const arr = ['-', ...firstNum];
-          setFirstNum(arr);
-          setView(arr);
+        if (!action && firstNum[0] !== '-' && firstNum) {
+          const str = '-'.concat(firstNum);
+          setFirstNum(str);
+          setView(str);
         } else if (!action) {
           const arr = firstNum.slice(1);
           setFirstNum(arr);
           setView(arr);
         } else if (action && secondNum[0] !== '-') {
-          const arr = ['-', ...secondNum];
-          setSecondNum(arr);
-          setView(arr);
+          const str = '-'.concat(secondNum);
+          setSecondNum(str);
+          setView(str);
         } else {
           const arr = secondNum.slice(1);
           setSecondNum(arr);
           setView(arr);
         }
+      } else if (val === '%') {
+        if (!action) {
+          const holder = firstNum / 100;
+          setFirstNum(holder);
+          setView(holder);
+        } else {
+          const holder = secondNum / 100;
+          setSecondNum('');
+          setView(firstNum);
+        }
       } else if (val === '=') {
-        const first = numify(firstNum);
-        const second = numify(secondNum);
-        const total = [operator[action](first, second).toString()];
+        const first = parseFloat(firstNum);
+        const second = parseFloat(secondNum);
+        const total = operator[action](first, second).toString();
         setView(total);
         setFirstNum(total);
-        setSecondNum([]);
+        setSecondNum('');
         setAction(null);
       }
     }
@@ -65,8 +75,6 @@ function App() {
   return (
     <>
       <div className=".App">
-        <View display={firstNum} />
-        <View display={secondNum} />
         <View display={view} />
         <ButtonList onClick={(name) => handleClick(name)} />
       </div>
