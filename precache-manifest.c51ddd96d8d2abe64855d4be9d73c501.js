cfec3c45e9b03abe2839c2c309fce39acf2e8a8d
Spencer Call <spencergeraldcall@gmail.com>
2020-05-07 12:56:38 -0600

    Updates

---
+++ precache-manifest.c51ddd96d8d2abe64855d4be9d73c501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007186915e226fec7552111570eee7e0",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "8df6777d1e8e3e4176dc",
+    "url": "/calc/static/css/main.3233b6ec.chunk.css"
+  },
+  {
+    "revision": "7c3b248bc5222e7bf2e3",
+    "url": "/calc/static/js/2.86d35a27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calc/static/js/2.86d35a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df6777d1e8e3e4176dc",
+    "url": "/calc/static/js/main.7192bb27.chunk.js"
+  },
+  {
+    "revision": "ef3484f9ac79d1f39869",
+    "url": "/calc/static/js/runtime-main.c61a5cac.js"
+  }
+]);
